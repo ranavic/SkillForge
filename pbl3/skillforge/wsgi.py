c8f7c41f8223d25a0f7f19f15a0ddd403ee0d7f4
Subham Ambastha <ambasthasubham@gmail.com>
2025-06-05 02:37:55 +0530

    Update wsgi.py

--- pbl3/skillforge/wsgi.py
+++ pbl3/skillforge/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for skillforge project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Add the project root (pbl3/) to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Set the Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skillforge.settings')
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skillforge.settings')
 
 application = get_wsgi_application()
